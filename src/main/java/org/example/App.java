1e12fc60020022d75d64435cfe148d97cc7cb6e2
Rodrigo Maliachi <roderykmaliachi@gmail.com>
2022-11-14 18:24:03 -0600

    Running Table View

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,11 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.example.gui.EmployeesTableView;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        EmployeesTableView.main(args);
     }
 }
