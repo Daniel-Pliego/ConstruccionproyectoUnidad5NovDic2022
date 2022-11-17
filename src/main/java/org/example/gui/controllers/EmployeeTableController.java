902d1bd0d21b670e94fdd90d5c67bacb58ecf0a5
Daniel-Pliego <a20216405@alumnos.uady.mx>
2022-11-17 15:58:07 -0600

    feat: conected view with JsonReader

--- src/main/java/org/example/gui/controllers/EmployeeTableController.java
+++ src/main/java/org/example/gui/controllers/EmployeeTableController.java
@@ -20,28 +20,9 @@ public class EmployeeTableController {
     
     private void initComponents() {
         table.setModel(
-            new EmployeeTableModel(
-                new Employee[] {
-                    new Employee(
-                        1,
-                        "Tom",
-                        "Cruise",
-                        "https://jsonformatter.org/img/tom-cruise.jpg"
-                    ),
-                    new Employee(
-                        2,
-                        "Maria",
-                        "Sharapova",
-                        "https://jsonformatter.org/img/Maria-Sharapova.jpg"
-                    ),
-                    new Employee(
-                        3,
-                        "Robert",
-                        "Downey Jr.",
-                        "https://jsonformatter.org/img/Robert-Downey-Jr.jpg"
-                    )
-                }
-            )
+            new EmployeeTableModel()
         );
     }
+
+
 }
