e698ed2d3038b41ad54fc8a529c1aec464694199
Rodrigo Maliachi <roderykmaliachi@gmail.com>
2022-11-24 17:42:19 -0600

    MVC edit employee

--- src/main/java/org/example/gui/EmployeesTableView.java
+++ src/main/java/org/example/gui/EmployeesTableView.java
@@ -1,5 +1,7 @@
 package org.example.gui;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JTable;
 
 public class EmployeesTableView extends javax.swing.JFrame {
@@ -10,6 +12,14 @@ public class EmployeesTableView extends javax.swing.JFrame {
 
     public JTable getEmployeesTable() {
         return employeesTable;
+    }
+
+    public JButton getEditButton() {
+        return editButton;
+    }
+
+    public JComboBox<String> getEmployeeComboBox() {
+        return employeeComboBox;
     }
 
     /**
@@ -24,6 +34,8 @@ public class EmployeesTableView extends javax.swing.JFrame {
         popupMenu1 = new java.awt.PopupMenu();
         scrollPane = new javax.swing.JScrollPane();
         employeesTable = new javax.swing.JTable();
+        employeeComboBox = new javax.swing.JComboBox<>();
+        editButton = new javax.swing.JButton();
 
         popupMenu1.setLabel("popupMenu1");
 
@@ -42,26 +54,52 @@ public class EmployeesTableView extends javax.swing.JFrame {
         ));
         scrollPane.setViewportView(employeesTable);
 
+        employeeComboBox.setEditable(true);
+        employeeComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        editButton.setText("Edit employee");
+        editButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                editButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 403, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 403, Short.MAX_VALUE)
+                    .addComponent(employeeComboBox, javax.swing.GroupLayout.Alignment.TRAILING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(editButton)
+                .addGap(144, 144, 144))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 346, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 307, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(employeeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(editButton))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void editButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_editButtonActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton editButton;
+    private javax.swing.JComboBox<String> employeeComboBox;
     private javax.swing.JTable employeesTable;
     private java.awt.PopupMenu popupMenu1;
     private javax.swing.JScrollPane scrollPane;
---
+++ src/main/java/org/example/gui/controllers/EditEmployeeController.java
@@ -0,0 +1,47 @@
+package org.example.gui.controllers;
+
+import org.example.gui.EditEmployeeView;
+import org.example.models.Employee;
+
+import javax.swing.*;
+
+public class EditEmployeeController {
+
+    private final JTextField firstNameField;
+    private final JTextField lastNameField;
+    private final JTextField fileNameField;
+
+    private final JButton editButton;
+
+    private Employee employee;
+
+    public EditEmployeeController(Employee employee) {
+
+        this.employee = employee;
+
+        EditEmployeeView view = new EditEmployeeView();
+        view.setVisible(true);
+
+        firstNameField = view.getFirstNameField();
+        lastNameField = view.getLastNameField();
+        fileNameField = view.getFileNameField();
+        editButton = view.getEditButton();
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        firstNameField.setText(employee.getFirstName());
+        lastNameField.setText(employee.getLastName());
+        fileNameField.setText(employee.getPhoto());
+
+        editButton.addActionListener(event -> {
+            employee = new Employee(
+                    employee.getId(),
+                    firstNameField.getText(),
+                    lastNameField.getText(),
+                    fileNameField.getText()
+            );
+        });
+    }
+}
