47cd087b45f044eb4963d217eef62a69f1968a49
Rodrigo Maliachi <roderykmaliachi@gmail.com>
2022-12-01 11:49:46 -0600

    Adding file chooser

--- src/main/java/org/example/gui/controllers/EditEmployeeController.java
+++ src/main/java/org/example/gui/controllers/EditEmployeeController.java
@@ -5,18 +5,19 @@ import org.example.models.Employee;
 import org.example.models.EmployeeDB;
 
 import javax.swing.*;
+import java.awt.event.*;
+import java.io.File;
 
 public class EditEmployeeController {
 
+    private final EditEmployeeView view;
     private final JTextField firstNameField;
     private final JTextField lastNameField;
     private final JTextField fileNameField;
-
     private final JButton editButton;
+    private final JFileChooser fileChooser = new JFileChooser();
 
     private Employee employee;
-
-    private final EditEmployeeView view;
 
     public EditEmployeeController(Employee employee) {
 
@@ -35,10 +36,13 @@ public class EditEmployeeController {
     }
 
     private void initComponents() {
-        firstNameField.setText(employee.getFirstName());
-        lastNameField.setText(employee.getLastName());
-        fileNameField.setText(employee.getPhoto());
+        initLabels();
+        initFileChooser();
+        addMouseListenerToFileNameField();
+        addActionListenerToEditButton();
+    }
 
+    private void addActionListenerToEditButton() {
         editButton.addActionListener(event -> {
             employee = new Employee(
                     employee.getId(),
@@ -49,6 +53,31 @@ public class EditEmployeeController {
             EmployeeDB.updateDB(employee);
             closeWindow();
         });
+    }
+
+    private void addMouseListenerToFileNameField() {
+        fileNameField.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                fileChooser.showDialog(view, null);
+            }
+        });
+    }
+
+    private void initFileChooser() {
+        fileChooser.addActionListener(e -> {
+            File file = fileChooser.getSelectedFile();
+
+            if (file != null) {
+                fileNameField.setText(file.getPath());
+            }
+        });
+    }
+
+    private void initLabels() {
+        firstNameField.setText(employee.getFirstName());
+        lastNameField.setText(employee.getLastName());
+        fileNameField.setText(employee.getPhoto());
     }
 
     private void closeWindow() {
